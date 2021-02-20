8755fe9ca9dcde14bbe09102e30346a80000491d
antonylineesh1 <antonylineesh1@gmail.com>
2021-02-20 23:14:24 +0530

    previous commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import './App.css';
+import React from 'react'
 import { useQuery } from 'react-query';
 import axios from 'axios';
 
-function App() {
+export function Pokemon() {
 
-  const queryInfo = useQuery('pokemon', async () => {
+  const queryInfo = usePokemon();
+
+  return (
+    <div>
+      {queryInfo.isLoading ?
+        'Loading...' :
+        queryInfo.isError ?
+          'Error ...' : <div>
+            {queryInfo.data.map(result => <div key={result.name}>{result.name}</div>)}
+          </div>}
+      {queryInfo.isFetching && 'Fetching...'}
+    </div>
+  )
+}
+
+function usePokemon() {
+  return useQuery('pokemon', async () => {
     await new Promise(resolve => setTimeout(resolve, 1000));
     return axios.get('https://pokeapi.co/api/v2/pokemon')
       .then(res => res.data.results);
   });
+}
+
+function Count() {
+  const queryInfo = usePokemon();
+  return (
+    <div>You are using this {queryInfo.data?.length} pokemons</div>
+  );
+}
+
+function App() {
 
   return (
-    <div className="App">
-      <h1>Welcome All</h1>
 
-      {queryInfo.isLoading ? 'Loading...' : <div>
-        {queryInfo.data.map(result => <div key={result.name}>{result.name}</div>)}
-      </div>}
+    <div className="App">
+      <Pokemon />
+      <Count/>
     </div>
+
   );
 }
 
